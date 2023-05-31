bbba8f1c6fbd3b85b64a809f303ca63a2fab1bde
Thomas Sarpong <thomas.sarpong@mpharma.com>
2023-05-31 13:42:33 +0000

    Removed unused code

--- src/app/http/router/introRoutes.ts
+++ src/app/http/router/introRoutes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { introControllerInstance } from "../controllers/instances";
-import { HttpBadRequestException } from "../../../exceptions";
 //All sub routes are supposed to be appended here
 
 const router = Router();
